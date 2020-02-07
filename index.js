d89c668a78d7dee79ccfc61a4dc2ab1aadbb7e13
Abdul <abdul@radadi.me>
2020-02-07 20:42:48 +0300

    add react-native-navigation for iOS and Android

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
-/**
- * @format
- */
-
-import {AppRegistry} from 'react-native';
+import {Navigation} from 'react-native-navigation';
 import App from './App';
-import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+Navigation.registerComponent('app', () => App);
+
+Navigation.events().registerAppLaunchedListener(() => {
+  Navigation.setRoot({
+    root: {
+      component: {
+        name: 'app',
+      },
+    },
+  });
+});
